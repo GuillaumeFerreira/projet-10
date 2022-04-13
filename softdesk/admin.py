5368152bca0ffdd5487aa8ad2198303c334ca0a9
ferreira <guillaumeferreira00@gmail.com>
2022-04-13 08:47:28 +0200

    initial commit

---
+++ softdesk/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from softdesk.models import Projects, Contributors, Issues, Comments
+
+
+
+
+class ProjectsAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'description')
+
+
+class ContributorsAdmin(admin.ModelAdmin):
+
+    list_display = ('user_id', 'role')
+
+
+class IssuesAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'author_user_id')
+
+
+class CommentsAdmin(admin.ModelAdmin):
+
+    list_display = ('author_user_id', 'description')
+
+
+admin.site.register(Projects, ProjectsAdmin)
+admin.site.register(Contributors, ContributorsAdmin)
+admin.site.register(Issues, IssuesAdmin)
+admin.site.register(Comments, CommentsAdmin)
---
+++ softdesk/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.conf import settings
+
+
+class Projects(models.Model):
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    type = models.CharField(max_length=255)
+    author_user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+
+
+class Contributors(models.Model):
+
+    user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(Projects, on_delete=models.CASCADE, related_name='projects_contributors')
+    #permission = models.ChoicesField()
+    role = models.CharField(max_length=255)
+
+
+
+
+class Issues(models.Model):
+
+    title = models.CharField(max_length=255)
+    author_user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    project_id = models.ForeignKey(Projects, on_delete=models.CASCADE, related_name='projects_issues')
+    tag = models.CharField(max_length=255)
+    priority = models.CharField(max_length=255)
+    #assignee_user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    status = models.CharField(max_length=255)
+    desc = models.CharField(max_length=255)
+
+
+
+
+class Comments(models.Model):
+
+    description = models.TextField(blank=True)
+    author_user_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    issue_id = models.ForeignKey(Issues, on_delete=models.CASCADE, related_name='issues')
+
+
