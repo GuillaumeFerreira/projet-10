54a36f9bbc216e1a8f135efd9dd12b894e3b9a1e
ferreira <guillaumeferreira00@gmail.com>
2022-04-13 09:55:52 +0200

    ajout authentification

--- project/urls.py
+++ project/urls.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
-from softdesk.views import ProjectsAPIView
+from softdesk.views import ProjectsViewset
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from rest_framework import routers
 
+# Ici nous créons notre routeur
+router = routers.SimpleRouter()
+# Puis lui déclarons une url basée sur le mot clé ‘category’ et notre view
+# afin que l’url générée soit celle que nous souhaitons ‘/api/category/’
+router.register('projects', ProjectsViewset, basename='projects')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api/projects/', ProjectsAPIView.as_view())
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/', include(router.urls))
 ]
--- softdesk/views.py
+++ softdesk/views.py
@@ -1,12 +1,27 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from softdesk.models import Projects
 from softdesk.serializers import ProjectsSerializer
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ReadOnlyModelViewSet
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth.models import User
 
+class ProjectsViewset(ReadOnlyModelViewSet):
 
-class ProjectsAPIView(APIView):
+    serializer_class = ProjectsSerializer
+    permission_classes = [IsAuthenticated]
 
-    def get(self, *args, **kwargs):
-        projects = Projects.objects.all()
-        serializer = ProjectsSerializer(projects, many=True)
-        return Response(serializer.data)
+    def get_queryset(self):
+        return Projects.objects.all()
+
+    def get(self, request, format=None):
+        content = {
+            'status': 'request was permitted'
+        }
+        return Response(content)
+
+    
+class UserViewset(ReadOnlyModelViewSet):
+
+    def get_queryset(self):
+        return User.objects.all()
